c5da1f4eb30c81b4b434a809ad0bd4d7a2002abc
Albert Remalin Raja <albertpaulus836@gmail.com>
2023-06-19 09:34:33 +0530

    test fetch function using random API

---
+++ test.js
@@ -0,0 +1,12 @@
+
+async function logJSONData() {
+    try {
+      const response = await fetch("https://randomuser.me/api/");
+      const jsonData = await response.json();
+      console.log(jsonData);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  logJSONData();
