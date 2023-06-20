c3874afdc722c1c0701ee4f19982c6638477a035
Albert Remalin Raja <albertpaulus836@gmail.com>
2023-06-20 13:40:20 +0530

    no changes

--- server.js
+++ server.js
@@ -1,27 +1,21 @@
 const http = require("http");
 const router = require("./routes/blogRoute");
 const mongoose = require("mongoose");
-mongoose
-  .connect("mongodb://0.0.0.0:27017/elora", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-  });
+const connection=require("./connection") 
+
+//db connection
+connection();
+
 
 const hostname = "127.0.0.1";
 const port = "3000";
 
 const server = http.createServer((req, res) => {
-  // res.statusCode=200;
-  // res.setHeader('Content-Type','text/plain');
-  // res.end("hello world")
-  console.log("hi", router);
+  res.statusCode=200;
+  res.setHeader('Content-Type','text/plain');
+  
   router(req, res);
+  // res.end() // learn this  `
 });
 
 server.listen(port, hostname, () => {
